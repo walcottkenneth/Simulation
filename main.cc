646eadf2a3f3bd9e6fbc499ff4bd7392ba6e6976
walcottkenneth <33814635+walcottkenneth@users.noreply.github.com>
2018-04-09 00:49:53 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,27 @@
 #include<iostream>
+#include "lost.h"
 using namespace std;
+const int MAX_Y, MAX_X = 10;
+
+void location() {
+node* head;
+head->new node;
+node* temp = head;
+int h = 0;
+while(h<MAX_Y){
+  for(int i= 0; i< MAX_X; i++){
+    temp.right->new node;
+    temp = temp->right;
+    i++;
+  }
+  temp = head;
+  if(temp.up == NULL){ 
+    temp.up->new node;
+    temp = temp->up;
+    h++;
+  }
+  else { temp = temp->up; }
+}
 
 int main() {
 
