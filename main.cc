834827730666db9bd8859cd9ac8755e15abd4921
walcottkenneth <33814635+walcottkenneth@users.noreply.github.com>
2018-04-11 15:34:01 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,38 +1,63 @@
 #include<iostream>
-#include "lost.h"
+#include<cmath>
+#include<ctime>
+#include<cstdlib>
+#include<ncurses.h>
 using namespace std;
-const int MAX_Y, MAX_X = 10;
 
-/*
-Location function creates the map that the user moves along. If done correctly,
-hopefully we can have a easy, medium, and hard setting in int main(), that would
-change the size of the map.
-*/
-// Just accepted how terrible of an idea this is, im going to have to switch it
-// out for something easier. Sorry.
-void location() {
-node* head;
-head = new node;
-node* temp = head;
-int h = 0;
-while(h<MAX_Y){
-  for(int i= 0; i< MAX_X; i++){
-    temp.right->new node;
-    node.left->temp;
-    temp = temp->right;
-    i++;
-  }
-  temp = head;
-  if(temp.up == NULL){ 
-    temp.up->new node;
-    node.down->temp;
-    temp = temp->up;
-    h++;
-  }
-  else { temp = temp->up; }
+/* const int UP = 65;
+ * const int DOWN = 66;
+ * const int LEFT = 68;
+ * const int RIGHT = 67;
+ * const MAX_X, MAX_Y;
+ *
+ */
+
+struct node{
+    int piece;
+    node() {}
+    node(int new_piece): piece(new_piece){}
+};
+
+void die() {
+    cout << "Gone but not forgotten!\n";
+    cout << "R.I.P.\n";
+    exit(1);
 }
 
+void location(int x, int y) {
+    while(x < 0) x+=MAX_X;
+    while(y < 0) y+=MAX_Y;
+    if(x >= MAX_X) x %= MAX_X;
+    if(y >= MAX_Y) y %= MAX_Y;
+    return (x*MAX_Y+x);
+}
+
+//void puzzle(){}
+
+unsigned struct* world;
+
+
 int main() {
+    //srand(time(NULL));
+
+    //Set up NCURSES
+    initscr();
+    clear();
+    noecho();:wq
+    cbreak();
+
+    world = new unsigned struct[MAX_X * MAX_Y];
+    //cout << "^L";
+    game_on = false;
+    while(true) {
+    cout << "DIDDY SAYS TAKE THAT.\n";
+    int ch = getch();
+    if(ch == 'q' or ch == 'Q') die();
+    /* else if (ch ==
+     *
+     */
+
   cout << "^.^.^.^.^.^.^.^.^.^.^.^.^.^.^.^.^.^.^
   cout << "-------------------------------------" << endl;
   cout << "-x--x--- Lost in Los Angeles ---x--x-" << endl;
